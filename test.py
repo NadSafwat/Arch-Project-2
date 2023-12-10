508a5eb76bf99a4da4b511cf550499034da03d60
NadSafwat <nadine.hkm@aucegypt.edu>
2023-12-10 23:52:26 +0200

    stuff

--- test.py
+++ test.py
@@ -26,16 +26,29 @@ RegisterStat = {
 ReservationStation = [LOAD1, LOAD2, STORE1, STORE2, ADD_ADDI1, ADD_ADDI2, ADD_ADDI3, NAND, DIV, BNE, CALL_RET]
 
 mem = []
+inst_issed = []
 inst_RS = []
 exec_time = []
 can_write = []
 write_queue = []
 instructions = []
 
+call_ret_issued = False
+call_ret_written = False
+stall_issuing = False
+
+branchIssued = False
+BranchWritten = False
+BranchIndex = 100000
+BranchTaken = False
+stall_executing = False
+
+Number_of_branches = 0
+Number_of_branches_taken = 0
 
 Reg = {
     "r0": 0,
-    "r1": 1,
+    "r1": 7,
     "r2": 0,
     "r3": 0,
     "r4": 0,
@@ -46,12 +59,15 @@ Reg = {
 }
 
 def IntializeMem ():
-    for i in range(8):
+    for i in range(6400):
         mem.append(0)
 
 def is_valid_instruction(instruction):
     if (instruction[0:4] == "load" or instruction[0:5] == "store"):
-       instruction_pattern = re.compile(r'(load|store)r[0-7],[0-9]+\(r[0-7]\)$')
+       if(instruction[7:9]=="32"):
+            return False
+       else:
+            instruction_pattern = re.compile(r'(load|store)r[0-7],(-?(?:[0-9]|[1-2][0-9]|3[0-2]))\(r[0-7]\)$')
     elif(instruction[0:3] == "bne"):
         instruction_pattern = re.compile(r'bner[0-7],r[0-7],[0-9]+$')
     elif(instruction[0:4] == "call"):
@@ -112,8 +128,8 @@ instructions = [["load", "r5", "0", "r2"],
                 ["load", "r2", "1", "r2"], 
                 ["div","r0","r2","r4"], 
                 ["add", "r4","r2","r6"], 
-                ["ret",None,None,None],
-                ["add", "r6", "r1","r5"] , 
+                ["bne","r1","r2","2"],
+                ["add", "r2", "r1","r5"], 
                 ["addi", "r1", "r1", "1"], 
                 ["add", "r6", "r1","r5"]]
 
@@ -187,9 +203,14 @@ def canIssue(Inst, written_FU):
         return False,None
 
 def issue(Inst, PC, written_FU):
+    global branchIssued
+    global BranchIndex
+    global call_ret_issued
+
+    issue_flag, issue_station = canIssue(Inst, written_FU)
     if(Inst[0] == "load"):
-        if(canIssue(Inst,written_FU)[0] == True):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag == True):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = Reg[Inst[3]]
@@ -205,6 +226,7 @@ def issue(Inst, PC, written_FU):
         
             RegisterStat[Inst[1]] = station["Name"]
             inst_RS.append(station)
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
             return True
@@ -212,8 +234,8 @@ def issue(Inst, PC, written_FU):
             return False
     
     elif(Inst[0] == "store"):
-        if(canIssue(Inst,written_FU)[0] == True ):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag == True ):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = Reg[Inst[1]]
@@ -232,15 +254,16 @@ def issue(Inst, PC, written_FU):
                 station["Qk"] = RegisterStat[Inst[1]]
 
             inst_RS.append(station)
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
             return True
         else:
-            return False         
+            return False       
 
     elif(Inst[0] == "add" or Inst[0] == "nand" or Inst[0] == "div"):
-        if(canIssue(Inst,written_FU)[0] == True):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag == True):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = Reg[Inst[2]]
@@ -260,16 +283,17 @@ def issue(Inst, PC, written_FU):
 
             RegisterStat[Inst[1]] = station["Name"]
             inst_RS.append(station) 
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
             return True
         else:
-            return False  
+            return False
                    
 
     elif(Inst[0] == "addi"):
-        if(canIssue(Inst,written_FU)[0] == True):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag == True):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = Reg[Inst[2]]
@@ -285,6 +309,7 @@ def issue(Inst, PC, written_FU):
 
             RegisterStat[Inst[1]] = station["Name"]
             inst_RS.append(station)
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
             return True
@@ -292,8 +317,8 @@ def issue(Inst, PC, written_FU):
             return False
 
     elif(Inst[0] == "bne"):
-        if(canIssue(Inst,written_FU)[0]==True):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag ==True):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = Reg[Inst[1]]
@@ -311,15 +336,19 @@ def issue(Inst, PC, written_FU):
                 station["Qk"] = RegisterStat[Inst[2]]
             
             inst_RS.append(station)
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
+
+            branchIssued = True
+            BranchIndex = (len(inst_issed) - 1)
             return True
         else:
             return False
 
     elif(Inst[0]=="call"):
-        if(canIssue(Inst,written_FU)[0] == True):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag == True):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = None
@@ -331,15 +360,18 @@ def issue(Inst, PC, written_FU):
 
             RegisterStat["r1"] = station["Name"]
             inst_RS.append(station)
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
+
+            call_ret_issued = True
             return True
         else:
             return False
 
     elif(Inst[0]=="ret"):
-        if(canIssue(Inst,written_FU)[0] == True):
-            station = canIssue(Inst,written_FU)[1]
+        if(issue_flag == True):
+            station = issue_station
             station["Busy"] = 'Y'
             station["OP"] = Inst[0]
             station["Vj"] = "r1"
@@ -352,8 +384,11 @@ def issue(Inst, PC, written_FU):
                 station["Vj"] = None
                 station["Qj"] = RegisterStat["r1"]
             inst_RS.append(station)
+            inst_issed.append(Inst)
             exec_time.append(0)
             can_write.append(0)
+
+            call_ret_issued = True
             return True
         else:
             return False
@@ -371,6 +406,8 @@ def removeInst(station):
     return station
     
 def canExecute(j, station, count):
+    global Number_of_branches
+
     if(station["OP"] == "load"):
         try:
             if(station["Qj"] is None and count < 3):
@@ -434,6 +471,7 @@ def canExecute(j, station, count):
             if(station["Qj"] is None and station["Qk"] is None and count < 1):
                 if(station["Vj"] != station["Vk"]):
                     station["A"] =  station["Imm"] + station["A"] + 1
+                Number_of_branches += 1
                 can_write[j] = 1
                 return (count+1)
             else:
@@ -453,20 +491,35 @@ def canExecute(j, station, count):
             return count
     
     elif(station["OP"] == "call"):
-        if(count < 1):
-            can_write[j] = 1
-            return (count+1)
-        else:
-            return count 
+        try:
+            if(count < 1):
+                can_write[j] = 1
+                return (count+1)
+            else:
+                return count
+        except:
+            return count
     
     elif(station["OP"] == "ret"):
-        if(station["Qj"] is None and count < 1):
-            can_write[j] = 1 
-            return (count+1)
-        else:
-            return count    
+        try:
+            if(station["Qj"] is None and count < 1):
+                can_write[j] = 1 
+                return (count+1)
+            else:
+                return count
+        except:
+            return count
 
-def WriteBack(Inst, station):
+    else:
+        return None
+
+def WriteBack(Inst, station,PC):
+    global BranchWritten
+    global BranchTaken
+    global Number_of_branches_taken
+    
+    global call_ret_written
+
     if(station["OP"] == "load"):
         if(Inst[1] != "r0"):
             Reg[Inst[1]] = mem[station["A"]]
@@ -503,63 +556,109 @@ def WriteBack(Inst, station):
         station = removeInst(station)
 
     elif(station["OP"] == "bne"):
+        if(station["Vj"] != station["Vk"]):
+            BranchTaken = True
+            Number_of_branches_taken += 1
+            PC = station["A"]
+        BranchWritten = True
         fix_after_write(station["Name"], None)
         station = removeInst(station)
 
     elif(station["OP"] == "call"):
         Reg["r1"] = station["Imm"]+1
+        PC = station["A"]
+        call_ret_written = True
         fix_after_write(station["Name"], "r1")
         station = removeInst(station)
 
     elif(station["OP"] == "ret"):
+        PC = station["A"]
+        call_ret_written = True
         fix_after_write(station["Name"], None)
         station = removeInst(station)
+    
+    return PC
 
 def simulate(clk, PC):
     written_FU = None
+    global inst_issed
+    global inst_RS
+    global stall_issuing
+    global stall_executing
+    global call_ret_written
+    global call_ret_issued
+    global branchIssued
+    global BranchWritten
+    global BranchIndex
+    global BranchTaken
+
+    stall_issuing = call_ret_issued and not call_ret_written
+    stall_executing = branchIssued and not BranchWritten
+
+    if(not stall_issuing):
+        call_ret_issued = False
+        call_ret_written = False
+    
+    if(not stall_executing):
+        branchIssued = False
+        BranchWritten = False
+        BranchIndex = 100000
+        BranchTaken = False
+
     if(clk > 1):
         if(len(write_queue)):
             i = write_queue[0]
             written_FU = inst_RS[i]["Name"]
-            WriteBack(instructions[i], inst_RS[i])
-            print("Inst ", i, "Written")
+            PC_target = WriteBack(inst_issed[i], inst_RS[i],PC)
+            print("Inst ", i, "Written")                
             write_queue.pop(0)
 
     if(clk != 0):
         for j in range(len(inst_RS)):
-            exec_time[j] = canExecute(j, inst_RS[j], exec_time[j])
-            print("Executed inst", j, " ", exec_time[j], " cycles")
-            if(can_write[j]):
-                write_queue.append(j)
-                can_write[j] = 0
+            if(j <= BranchIndex):
+                exec_time[j] = canExecute(j, inst_RS[j], exec_time[j])
+                print("Executed inst", inst_issed[j][0] , " ", exec_time[j], " cycles")
+                if(can_write[j]):
+                    write_queue.append(j)
+                    can_write[j] = 0
 
     if(PC < len(instructions)):
-        if(issue(instructions[PC], PC, written_FU)):
-            print("Issued Inst ", PC)
-            return(PC + 1)
+        if (not stall_issuing):
+            issue_flag = issue(instructions[PC], PC, written_FU)
+            if(issue_flag):
+                print("Issued Inst ", PC)
+        if(BranchTaken):
+            inst_issed = inst_issed[:BranchIndex+1]
+            inst_RS = inst_RS[:BranchIndex+1]
+            return PC_target
+        elif(stall_issuing):
+            return (PC)
         else:
-            return PC
+            return(PC+1)
     else:
         return PC
+    
         
 def top ():
     clk = 0
     PC = 0
 
-    print(instructions)
-    for i in range (30):
-        PC = simulate(clk, PC)
-        print("==============================================")
-        clk += 1
-
-    #print(Reg)
-    # while(True):
-    #     go = input()
-    #     if(go):
-    #         PC = simulate(clk, PC)
-    #         clk += 1
+    while(True):
+        go = input()
+        if(go):
+            PC = simulate(clk, PC)
+            print("==============================================")
+            clk += 1
+        else:
+            return                  
 
 top()
 
 print(Reg)
-print(mem)
+# print(mem)
+print(inst_issed)
+
+print("Number of Branches: ", Number_of_branches)
+print("Number of Branches Taken: ", Number_of_branches_taken)
+
+print("IC: ", len(inst_issed))
